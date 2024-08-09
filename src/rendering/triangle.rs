9133cce4f45e3a7870ea6433a9dbb5d0fea404bb
Sergey Petrov <supudo@gmail.com>
2024-08-09 11:26:20 +0300

    rev

--- src/rendering/triangle.rs
+++ src/rendering/triangle.rs
@@ -16,9 +16,7 @@ pub mod gl {
 pub fn gl_config_picker(configs: Box<dyn Iterator<Item = Config> + '_>) -> Config {
   configs
     .reduce(|accum, config| {
-      let transparency_check = config.supports_transparency().unwrap_or(false)
-        & !accum.supports_transparency().unwrap_or(false);
-
+      let transparency_check = config.supports_transparency().unwrap_or(false) & !accum.supports_transparency().unwrap_or(false);
       if transparency_check || config.num_samples() > accum.num_samples() {
         config
       }
