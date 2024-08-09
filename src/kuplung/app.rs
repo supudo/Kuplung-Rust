8b893eff86707082b52f2f3243c79135e313d937
Sergey Petrov <supudo@gmail.com>
2024-08-09 11:16:40 +0300

    rev

--- src/kuplung/app.rs
+++ src/kuplung/app.rs
@@ -24,12 +24,6 @@ use crate::rendering::triangle;
 
 use log::{info, logger, warn};
 
-pub mod gl {
-  #![allow(clippy::all)]
-  include!(concat!(env!("OUT_DIR"), "/gl_bindings.rs"));
-  pub use Gles2 as Gl;
-}
-
 fn load_icon() -> Icon {
   let data_icon = include_bytes!(concat!(env!("OUT_DIR"), "/assets/Kuplung.png")).as_ref();
   let (icon_rgba, icon_width, icon_height) = {
@@ -74,7 +68,7 @@ impl ApplicationHandler for App {
 
     info!("[Kuplung] Initializing...");
 
-    let (mut window, gl_config) = match self.display_builder.clone().build(event_loop, self.template.clone(), gl_config_picker) {
+    let (mut window, gl_config) = match self.display_builder.clone().build(event_loop, self.template.clone(), triangle::gl_config_picker) {
       Ok(ok) => ok,
       Err(e) => {
         log::error!("[Kuplung] Error building the display! {}", e.to_string());
@@ -125,7 +119,7 @@ impl ApplicationHandler for App {
     // The context needs to be current for the Renderer to set up shaders and
     // buffers. It also performs function loading, which needs a current context on
     // WGL.
-    self.renderer.get_or_insert_with(|| Renderer::new(&gl_display));
+    self.renderer.get_or_insert_with(|| triangle::Renderer::new(&gl_display));
 
     if let Err(res) = gl_surface.set_swap_interval(&gl_context, SwapInterval::Wait(NonZeroU32::new(1).unwrap())) {
       log::error!("[Kuplung] Error setting vsync: {res:?}");
@@ -175,7 +169,7 @@ struct App {
   display_builder: DisplayBuilder,
   exit_state: Result<(), Box<dyn Error>>,
   gl_context: Option<NotCurrentContext>,
-  renderer: Option<Renderer>,
+  renderer: Option<triangle::Renderer>,
   // NOTE: `AppState` carries the `Window`, thus it should be dropped after everything else.
   state: Option<AppState>,
 }
@@ -200,163 +194,3 @@ struct AppState {
   // raw-window-handle.
   window: Window,
 }
-
-// Find the config with the maximum number of samples, so our triangle will be smooth.
-pub fn gl_config_picker(configs: Box<dyn Iterator<Item = Config> + '_>) -> Config {
-  configs
-    .reduce(|accum, config| {
-      let transparency_check = config.supports_transparency().unwrap_or(false)
-        & !accum.supports_transparency().unwrap_or(false);
-
-      if transparency_check || config.num_samples() > accum.num_samples() {
-        config
-      }
-      else {
-        accum
-      }
-    })
-    .unwrap()
-}
-
-pub struct Renderer {
-  program: gl::types::GLuint,
-  vao: gl::types::GLuint,
-  vbo: gl::types::GLuint,
-  gl: gl::Gl,
-}
-
-impl Renderer {
-  pub fn new<D: GlDisplay>(gl_display: &D) -> Self {
-    unsafe {
-      let gl = gl::Gl::load_with(|symbol| {
-        let symbol = CString::new(symbol).unwrap();
-        gl_display.get_proc_address(symbol.as_c_str()).cast()
-      });
-
-      if let Some(renderer) = get_gl_string(&gl, gl::RENDERER) {
-        info!("[Kuplung] Running on {}", renderer.to_string_lossy());
-      }
-      if let Some(version) = get_gl_string(&gl, gl::VERSION) {
-        info!("[Kuplung] OpenGL Version {}", version.to_string_lossy());
-      }
-
-      if let Some(shaders_version) = get_gl_string(&gl, gl::SHADING_LANGUAGE_VERSION) {
-        info!("[Kuplung] Shaders version on {}", shaders_version.to_string_lossy());
-      }
-
-      let vertex_shader = create_shader(&gl, gl::VERTEX_SHADER, triangle::VERTEX_SHADER_SOURCE);
-      let fragment_shader = create_shader(&gl, gl::FRAGMENT_SHADER, triangle::FRAGMENT_SHADER_SOURCE);
-
-      let program = gl.CreateProgram();
-
-      gl.AttachShader(program, vertex_shader);
-      gl.AttachShader(program, fragment_shader);
-
-      gl.LinkProgram(program);
-
-      gl.UseProgram(program);
-
-      gl.DeleteShader(vertex_shader);
-      gl.DeleteShader(fragment_shader);
-
-      let mut vao = std::mem::zeroed();
-      gl.GenVertexArrays(1, &mut vao);
-      gl.BindVertexArray(vao);
-
-      let mut vbo = std::mem::zeroed();
-      gl.GenBuffers(1, &mut vbo);
-      gl.BindBuffer(gl::ARRAY_BUFFER, vbo);
-      gl.BufferData(
-        gl::ARRAY_BUFFER,
-        (triangle::VERTEX_DATA.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr,
-        triangle::VERTEX_DATA.as_ptr() as *const _,
-        gl::STATIC_DRAW,
-      );
-
-      let pos_attrib = gl.GetAttribLocation(program, b"position\0".as_ptr() as *const _);
-      let color_attrib = gl.GetAttribLocation(program, b"color\0".as_ptr() as *const _);
-      gl.VertexAttribPointer(
-        pos_attrib as gl::types::GLuint,
-        2,
-        gl::FLOAT,
-        0,
-        5 * std::mem::size_of::<f32>() as gl::types::GLsizei,
-        std::ptr::null(),
-      );
-      gl.VertexAttribPointer(
-        color_attrib as gl::types::GLuint,
-        3,
-        gl::FLOAT,
-        0,
-        5 * std::mem::size_of::<f32>() as gl::types::GLsizei,
-        (2 * std::mem::size_of::<f32>()) as *const () as *const _,
-      );
-      gl.EnableVertexAttribArray(pos_attrib as gl::types::GLuint);
-      gl.EnableVertexAttribArray(color_attrib as gl::types::GLuint);
-
-      Self { program, vao, vbo, gl }
-    }
-  }
-
-  pub fn draw(&self) {
-    self.draw_with_clear_color(configuration::GL_CLEAR_COLOR_R, configuration::GL_CLEAR_COLOR_G, configuration::GL_CLEAR_COLOR_B, configuration::GL_CLEAR_COLOR_A)
-  }
-
-  pub fn draw_with_clear_color(
-    &self,
-    red: GLfloat,
-    green: GLfloat,
-    blue: GLfloat,
-    alpha: GLfloat,
-  ) {
-    unsafe {
-      self.gl.UseProgram(self.program);
-
-      self.gl.BindVertexArray(self.vao);
-      self.gl.BindBuffer(gl::ARRAY_BUFFER, self.vbo);
-
-      self.gl.ClearColor(red, green, blue, alpha);
-      self.gl.Clear(gl::COLOR_BUFFER_BIT);
-      self.gl.DrawArrays(gl::TRIANGLES, 0, 3);
-    }
-  }
-
-  pub fn resize(&self, width: i32, height: i32) {
-    unsafe {
-      self.gl.Viewport(0, 0, width, height);
-    }
-  }
-}
-
-impl Deref for Renderer {
-  type Target = gl::Gl;
-
-  fn deref(&self) -> &Self::Target {
-    &self.gl
-  }
-}
-
-impl Drop for Renderer {
-  fn drop(&mut self) {
-    unsafe {
-      self.gl.DeleteProgram(self.program);
-      self.gl.DeleteBuffers(1, &self.vbo);
-      self.gl.DeleteVertexArrays(1, &self.vao);
-    }
-  }
-}
-
-unsafe fn create_shader(gl: &gl::Gl, shader: gl::types::GLenum, source: &[u8]) -> gl::types::GLuint {
-  let shader = gl.CreateShader(shader);
-  gl.ShaderSource(shader, 1, [source.as_ptr().cast()].as_ptr(), std::ptr::null());
-  gl.CompileShader(shader);
-  shader
-}
-
-fn get_gl_string(gl: &gl::Gl, variant: gl::types::GLenum) -> Option<&'static CStr> {
-  unsafe {
-    let s = gl.GetString(variant);
-    (!s.is_null()).then(|| CStr::from_ptr(s.cast()))
-  }
-}
-
--- src/rendering/triangle.rs
+++ src/rendering/triangle.rs
@@ -1,3 +1,193 @@
+use std::error::Error;
+use std::ffi::{CStr, CString};
+use std::num::NonZeroU32;
+use std::ops::Deref;
+use env_logger::Env;
+use gl::types::GLfloat;
+use raw_window_handle::HasWindowHandle;
+use winit::application::ApplicationHandler;
+use winit::event::{KeyEvent, WindowEvent};
+use winit::keyboard::{Key, NamedKey};
+use winit::window::{Icon, Window};
+
+use glutin::config::{Config, ConfigTemplateBuilder};
+use glutin::context::{ContextApi, ContextAttributesBuilder, GlProfile, NotCurrentContext, PossiblyCurrentContext, Version};
+use glutin::display::GetGlDisplay;
+use glutin::prelude::*;
+use glutin::surface::{Surface, SwapInterval, WindowSurface};
+
+use glutin_winit::{DisplayBuilder, GlWindow};
+use log::info;
+use winit::dpi::{LogicalPosition, Position};
+use crate::rendering::triangle;
+use crate::settings::configuration;
+
+pub mod gl {
+  #![allow(clippy::all)]
+  include!(concat!(env!("OUT_DIR"), "/gl_bindings.rs"));
+  pub use Gles2 as Gl;
+}
+
+
+// Find the config with the maximum number of samples, so our triangle will be smooth.
+pub fn gl_config_picker(configs: Box<dyn Iterator<Item = Config> + '_>) -> Config {
+  configs
+    .reduce(|accum, config| {
+      let transparency_check = config.supports_transparency().unwrap_or(false)
+        & !accum.supports_transparency().unwrap_or(false);
+
+      if transparency_check || config.num_samples() > accum.num_samples() {
+        config
+      }
+      else {
+        accum
+      }
+    })
+    .unwrap()
+}
+
+pub struct Renderer {
+  program: gl::types::GLuint,
+  vao: gl::types::GLuint,
+  vbo: gl::types::GLuint,
+  gl: gl::Gl,
+}
+
+impl Renderer {
+  pub fn new<D: GlDisplay>(gl_display: &D) -> Self {
+    unsafe {
+      let gl = gl::Gl::load_with(|symbol| {
+        let symbol = CString::new(symbol).unwrap();
+        gl_display.get_proc_address(symbol.as_c_str()).cast()
+      });
+
+      if let Some(renderer) = get_gl_string(&gl, gl::RENDERER) {
+        info!("[Kuplung] Running on {}", renderer.to_string_lossy());
+      }
+      if let Some(version) = get_gl_string(&gl, gl::VERSION) {
+        info!("[Kuplung] OpenGL Version {}", version.to_string_lossy());
+      }
+
+      if let Some(shaders_version) = get_gl_string(&gl, gl::SHADING_LANGUAGE_VERSION) {
+        info!("[Kuplung] Shaders version on {}", shaders_version.to_string_lossy());
+      }
+
+      let vertex_shader = create_shader(&gl, gl::VERTEX_SHADER, triangle::VERTEX_SHADER_SOURCE);
+      let fragment_shader = create_shader(&gl, gl::FRAGMENT_SHADER, triangle::FRAGMENT_SHADER_SOURCE);
+
+      let program = gl.CreateProgram();
+
+      gl.AttachShader(program, vertex_shader);
+      gl.AttachShader(program, fragment_shader);
+
+      gl.LinkProgram(program);
+
+      gl.UseProgram(program);
+
+      gl.DeleteShader(vertex_shader);
+      gl.DeleteShader(fragment_shader);
+
+      let mut vao = std::mem::zeroed();
+      gl.GenVertexArrays(1, &mut vao);
+      gl.BindVertexArray(vao);
+
+      let mut vbo = std::mem::zeroed();
+      gl.GenBuffers(1, &mut vbo);
+      gl.BindBuffer(gl::ARRAY_BUFFER, vbo);
+      gl.BufferData(
+        gl::ARRAY_BUFFER,
+        (triangle::VERTEX_DATA.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr,
+        triangle::VERTEX_DATA.as_ptr() as *const _,
+        gl::STATIC_DRAW,
+      );
+
+      let pos_attrib = gl.GetAttribLocation(program, b"position\0".as_ptr() as *const _);
+      let color_attrib = gl.GetAttribLocation(program, b"color\0".as_ptr() as *const _);
+      gl.VertexAttribPointer(
+        pos_attrib as gl::types::GLuint,
+        2,
+        gl::FLOAT,
+        0,
+        5 * std::mem::size_of::<f32>() as gl::types::GLsizei,
+        std::ptr::null(),
+      );
+      gl.VertexAttribPointer(
+        color_attrib as gl::types::GLuint,
+        3,
+        gl::FLOAT,
+        0,
+        5 * std::mem::size_of::<f32>() as gl::types::GLsizei,
+        (2 * std::mem::size_of::<f32>()) as *const () as *const _,
+      );
+      gl.EnableVertexAttribArray(pos_attrib as gl::types::GLuint);
+      gl.EnableVertexAttribArray(color_attrib as gl::types::GLuint);
+
+      Self { program, vao, vbo, gl }
+    }
+  }
+
+  pub fn draw(&self) {
+    self.draw_with_clear_color(configuration::GL_CLEAR_COLOR_R, configuration::GL_CLEAR_COLOR_G, configuration::GL_CLEAR_COLOR_B, configuration::GL_CLEAR_COLOR_A)
+  }
+
+  pub fn draw_with_clear_color(
+    &self,
+    red: ::gl::types::GLfloat,
+    green: ::gl::types::GLfloat,
+    blue: ::gl::types::GLfloat,
+    alpha: ::gl::types::GLfloat,
+  ) {
+    unsafe {
+      self.gl.UseProgram(self.program);
+
+      self.gl.BindVertexArray(self.vao);
+      self.gl.BindBuffer(gl::ARRAY_BUFFER, self.vbo);
+
+      self.gl.ClearColor(red, green, blue, alpha);
+      self.gl.Clear(gl::COLOR_BUFFER_BIT);
+      self.gl.DrawArrays(gl::TRIANGLES, 0, 3);
+    }
+  }
+
+  pub fn resize(&self, width: i32, height: i32) {
+    unsafe {
+      self.gl.Viewport(0, 0, width, height);
+    }
+  }
+}
+
+impl Deref for Renderer {
+  type Target = gl::Gl;
+
+  fn deref(&self) -> &Self::Target {
+    &self.gl
+  }
+}
+
+impl Drop for Renderer {
+  fn drop(&mut self) {
+    unsafe {
+      self.gl.DeleteProgram(self.program);
+      self.gl.DeleteBuffers(1, &self.vbo);
+      self.gl.DeleteVertexArrays(1, &self.vao);
+    }
+  }
+}
+
+unsafe fn create_shader(gl: &gl::Gl, shader: gl::types::GLenum, source: &[u8]) -> gl::types::GLuint {
+  let shader = gl.CreateShader(shader);
+  gl.ShaderSource(shader, 1, [source.as_ptr().cast()].as_ptr(), std::ptr::null());
+  gl.CompileShader(shader);
+  shader
+}
+
+fn get_gl_string(gl: &gl::Gl, variant: gl::types::GLenum) -> Option<&'static CStr> {
+  unsafe {
+    let s = gl.GetString(variant);
+    (!s.is_null()).then(|| CStr::from_ptr(s.cast()))
+  }
+}
+
 #[rustfmt::skip]
 pub static VERTEX_DATA: [f32; 15] = [
   -0.5, -0.5,  1.0,  0.0,  0.0,
