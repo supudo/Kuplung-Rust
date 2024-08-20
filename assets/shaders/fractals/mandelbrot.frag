6d9f8c57ba1d8fcd626a17c0a67c6038ea1c158f
supudo <supudo@gmail.com>
2024-08-20 19:56:58 +0300

    rev

--- assets/shaders/fractals/mandelbrot.frag
+++ assets/shaders/fractals/mandelbrot.frag
@@ -10,12 +10,12 @@ vec4 mandelbrot_color(vec4 v_position);
 
 void main() {
     //FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
-    FragColor = vec4(1.0, (mod(gl_FragCoord.y, 256) / 256), 1.0, 1.0);
-    //FragColor = mandelbrot_color(fs_position);
+    //FragColor = vec4(1.0, (mod(gl_FragCoord.y, 256) / 256), 1.0, 1.0);
+    FragColor = mandelbrot_color(fs_position);
 }
 
-vec4 mandelbrot_color(vec4 v_position) {
-    vec2 c = v_position.xy / fs_window_height * 4.0 - 2.0;
+vec4 mandelbrot_color(vec4 vpos) {
+    vec2 c = vpos.xy / 1 * 4.0 - 2.0;
 
     vec2 z = c;
     float i;
@@ -24,7 +24,7 @@ vec4 mandelbrot_color(vec4 v_position) {
     }
 
     vec4 resultColor = vec4(0.0);
-    if (length(z) <= 2.0) {
+    if (length(z) <= 1.0) {
         resultColor = vec4(0.0, 0.0, 0.0, 1.0);
     }
     else {
--- src/fractals/fractals_manager.rs
+++ src/fractals/fractals_manager.rs
@@ -10,82 +10,80 @@ use crate::fractals::mandelbrot::Mandelbrot;
 use crate::settings::configuration;
 
 pub struct FractalsManager {
-    pub show_fractals: bool,
-    show_mandelbrot: bool,
-    fractal_mandelbrot: Arc<Mutex<Mandelbrot>>,
+  pub show_fractals: bool,
+  show_mandelbrot: bool,
+  fractal_mandelbrot: Arc<Mutex<Mandelbrot>>,
 }
 
 impl FractalsManager {
-    pub fn new<'a>(cc: &'a eframe::CreationContext<'a>) -> Option<Self> {
-        info!("[Kuplung] New FractalsManager...");
+  pub fn new<'a>(cc: &'a eframe::CreationContext<'a>) -> Option<Self> {
+    info!("[Kuplung] New FractalsManager...");
 
-        let gl = cc.gl.as_ref()?;
-        let this = Self {
-            show_fractals: false,
-            show_mandelbrot: true,
-            fractal_mandelbrot: Arc::new(Mutex::new(Mandelbrot::new(gl)?)),
-        };
+    let gl = cc.gl.as_ref()?;
+    let this = Self {
+      show_fractals: false,
+      show_mandelbrot: true,
+      fractal_mandelbrot: Arc::new(Mutex::new(Mandelbrot::new(gl)?)),
+    };
 
-        info!("[Kuplung] New FractalsManager finished.");
-        Some(this)
-    }
+    info!("[Kuplung] New FractalsManager finished.");
+    Some(this)
+  }
 
-    fn paint_mandelbrot(&mut self, ui: &mut Ui) {
-        ui.horizontal(|ui| {
-            ui.spacing_mut().item_spacing.x = 0.0;
-            ui.label("Mandelbrot");
-        });
-
-        egui::Frame::canvas(ui.style()).show(ui, |ui| {
-            let (rect, _) = ui.allocate_exact_size(egui::Vec2::splat(300.0), egui::Sense::drag());
-            let rotating_triangle = self.fractal_mandelbrot.clone();
-            let screen_width: f32 = ui.ctx().screen_rect().size().x;
-            let screen_height: f32 = ui.ctx().screen_rect().size().y;
-            let cb = egui_glow::CallbackFn::new(move |_info, painter| {
-                rotating_triangle.lock().paint(painter.gl(), screen_width, screen_height);
-            });
-            let callback = egui::PaintCallback {
-                rect,
-                callback: Arc::new(cb),
-            };
-            ui.painter().add(callback);
-        });
-        ui.label("Drag to rotate!");
-    }
+  fn paint_mandelbrot(&mut self, ui: &mut Ui) {
+    ui.horizontal(|ui| {
+      ui.spacing_mut().item_spacing.x = 0.0;
+      ui.label("Mandelbrot fractal");
+    });
+    egui::Frame::canvas(ui.style()).show(ui, |ui| {
+      let window_width: f32 = ui.available_size().x;
+      let window_height: f32 = ui.available_size().y;
+      let (rect, _) = ui.allocate_exact_size(egui::Vec2::from([window_width, window_height]), egui::Sense::drag());
+      let rotating_triangle = self.fractal_mandelbrot.clone();
+      let cb = egui_glow::CallbackFn::new(move |_, painter| {
+        rotating_triangle.lock().paint(painter.gl(), window_width, window_height);
+      });
+      let callback = egui::PaintCallback {
+        rect,
+        callback: Arc::new(cb),
+      };
+      ui.painter().add(callback);
+    });
+  }
 }
 
 impl eframe::App for FractalsManager {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        self.show_fractals = true;
-        egui::Window::new("Fractals")
-          .id(egui::Id::new("window_fractals"))
-          .resizable(true)
-          .enabled(true)
-          .default_pos([80.0, 80.0])
-          .min_size([configuration::WINDOW_POSITION_WIDTH_FRACTALS, 200.0])
-          .default_size([configuration::WINDOW_POSITION_WIDTH_FRACTALS, configuration::WINDOW_POSITION_HEIGHT_FRACTALS])
-          .show(ctx, |ui| {
-              egui::menu::bar(ui, |ui| {
-                  if ui.button("Close").clicked() {
-                      ui.close_menu();
-                      self.show_fractals = false;
-                  }
-                  ui.menu_button("Fractals", |ui| {
-                      if ui.button("Mandelbrot").clicked() {
-                          ui.close_menu();
-                          self.show_mandelbrot = true;
-                      }
-                  });
-              });
-              ui.separator();
-              if self.show_mandelbrot { self.paint_mandelbrot(ui); }
-              if !self.show_mandelbrot { ui.label("Select fractal from the menu."); }
+  fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+    self.show_fractals = true;
+    egui::Window::new("Fractals")
+      .id(egui::Id::new("window_fractals"))
+      .resizable(true)
+      .enabled(true)
+      .default_pos([80.0, 80.0])
+      .min_size([configuration::WINDOW_POSITION_WIDTH_FRACTALS, 200.0])
+      .default_size([configuration::WINDOW_POSITION_WIDTH_FRACTALS, configuration::WINDOW_POSITION_HEIGHT_FRACTALS])
+      .show(ctx, |ui| {
+        egui::menu::bar(ui, |ui| {
+          if ui.button("Close").clicked() {
+            ui.close_menu();
+            self.show_fractals = false;
+          }
+          ui.menu_button("Fractals", |ui| {
+            if ui.button("Mandelbrot").clicked() {
+              ui.close_menu();
+              self.show_mandelbrot = true;
+            }
           });
-    }
+        });
+        ui.separator();
+        if self.show_mandelbrot { self.paint_mandelbrot(ui); }
+        if !self.show_mandelbrot { ui.label("Select fractal from the menu."); }
+      });
+  }
 
-    fn on_exit(&mut self, gl: Option<&glow::Context>) {
-        if let Some(gl) = gl {
-            self.fractal_mandelbrot.lock().destroy(gl);
-        }
+  fn on_exit(&mut self, gl: Option<&glow::Context>) {
+    if let Some(gl) = gl {
+      self.fractal_mandelbrot.lock().destroy(gl);
     }
+  }
 }
