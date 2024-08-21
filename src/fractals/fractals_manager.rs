ae1a0aa265288088e78b7d049bc95cc31bfbe023
Sergey petrov <supudo@gmail.com>
2024-08-21 10:57:19 +0300

    rev

--- src/fractals/fractals_manager.rs
+++ src/fractals/fractals_manager.rs
@@ -16,6 +16,8 @@ pub struct FractalsManager {
   show_julia: bool,
   fractal_mandelbrot: Arc<Mutex<Mandelbrot>>,
   fractal_julia: Arc<Mutex<Julia>>,
+  option_mandelbrot_iterations: i32,
+  option_julia_iterations: i32,
 }
 
 impl FractalsManager {
@@ -25,10 +27,12 @@ impl FractalsManager {
     let gl = cc.gl.as_ref()?;
     let this = Self {
       show_fractals: false,
-      show_mandelbrot: false,
-      show_julia: true,
+      show_mandelbrot: true,
+      show_julia: false,
       fractal_mandelbrot: Arc::new(Mutex::new(Mandelbrot::new(gl)?)),
       fractal_julia: Arc::new(Mutex::new(Julia::new(gl)?)),
+      option_mandelbrot_iterations: 100,
+      option_julia_iterations: 256,
     };
 
     info!("[Kuplung] New FractalsManager finished.");
@@ -36,17 +40,20 @@ impl FractalsManager {
   }
 
   fn paint_mandelbrot(&mut self, ui: &mut Ui) {
+    ui.label("Mandelbrot fractal");
     ui.horizontal(|ui| {
-      ui.spacing_mut().item_spacing.x = 0.0;
-      ui.label("Mandelbrot fractal");
+      ui.label("Iterations:");
+      ui.add(egui::DragValue::new(&mut self.option_mandelbrot_iterations).speed(1.0));
     });
+    ui.separator();
+    let iterations = self.option_mandelbrot_iterations;
     egui::Frame::canvas(ui.style()).show(ui, |ui| {
       let window_width: f32 = ui.available_size().x;
       let window_height: f32 = ui.available_size().y;
       let (rect, _) = ui.allocate_exact_size(egui::Vec2::from([window_width, window_height]), egui::Sense::drag());
       let fractal_mandelbrot = self.fractal_mandelbrot.clone();
       let cb = egui_glow::CallbackFn::new(move |_, painter| {
-        fractal_mandelbrot.lock().paint(painter.gl(), window_width, window_height, 100);
+        fractal_mandelbrot.lock().paint(painter.gl(), window_width, window_height, iterations);
       });
       let callback = egui::PaintCallback {
         rect,
@@ -57,17 +64,20 @@ impl FractalsManager {
   }
 
   fn paint_julia(&mut self, ui: &mut Ui) {
+    ui.label("Julia fractal");
     ui.horizontal(|ui| {
-      ui.spacing_mut().item_spacing.x = 0.0;
-      ui.label("Julia fractal");
+      ui.label("Iterations:");
+      ui.add(egui::DragValue::new(&mut self.option_julia_iterations).speed(1.0));
     });
+    ui.separator();
+    let iterations = self.option_julia_iterations;
     egui::Frame::canvas(ui.style()).show(ui, |ui| {
       let window_width: f32 = ui.available_size().x;
       let window_height: f32 = ui.available_size().y;
       let (rect, _) = ui.allocate_exact_size(egui::Vec2::from([window_width, window_height]), egui::Sense::drag());
       let fractal_julia = self.fractal_julia.clone();
       let cb = egui_glow::CallbackFn::new(move |_, painter| {
-        fractal_julia.lock().paint(painter.gl(), window_width, window_height, 256);
+        fractal_julia.lock().paint(painter.gl(), window_width, window_height, iterations);
       });
       let callback = egui::PaintCallback {
         rect,
